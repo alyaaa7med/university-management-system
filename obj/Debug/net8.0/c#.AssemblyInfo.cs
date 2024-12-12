35823a63d69e7db196f5f38b52713c6527d392bd
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-12-12 22:42:09 +0200

    change the uml photo

--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240c3acc50e2496cd2b21a2aed6ade9263cdda32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860e1737db193ad0c5a6a83e65e1b2af7984268c")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
