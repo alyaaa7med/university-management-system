7ad8f9c2eb64ca1a9d6fcd648922668a8b29e9a1
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-12-16 19:05:44 +0200

    change picture

--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c4cf41037fe4c74dbacef97772fcf659a0bf54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6803922e394465856aef88d483890f20caca041d")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
